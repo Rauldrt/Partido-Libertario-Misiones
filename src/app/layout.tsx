3904bce66c2b4a8f7dac0ee376aa9082efa3ddba
La Libertad Avanza <laliavwanda@gmail.com>
2025-06-21 14:08:46 +0000

    que el purpura sea lo mas oscuro posible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased flex flex-col min-h-screen bg-gradient-to-b from-purple-900 to-fuchsia-800">
+      <body className="font-body antialiased flex flex-col min-h-screen bg-gradient-to-b from-[#1c032f] to-fuchsia-800">
         <Header />
         <div className="flex-grow flex flex-col perspective-container">
           <main className="flex-grow">
