6a593aff97a974f90805258dc9f16bd8e56e44de
La Libertad Avanza <laliavwanda@gmail.com>
2025-06-21 14:01:04 +0000

    cambiemos el gradiente del color del body de la pagina, que sea un purpu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased flex flex-col min-h-screen bg-gradient-to-br from-purple-900 to-rose-500">
+      <body className="font-body antialiased flex flex-col min-h-screen bg-gradient-to-b from-purple-900 to-fuchsia-800">
         <Header />
         <div className="flex-grow flex flex-col perspective-container">
           <main className="flex-grow">
