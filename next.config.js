ef1697fbb67dd8c519bfb77b70f98b4451b07e0c
La Libertad Avanza <laliavwanda@gmail.com>
2025-07-06 21:11:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -32,6 +32,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.infobae.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
