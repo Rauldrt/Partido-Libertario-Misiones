db0b3316cbf5e9d0799ba62bdd345c3f61e17627
La Libertad Avanza <laliavwanda@gmail.com>
2025-07-07 12:09:26 +0000

    que el gradiente inicie con un purpura los mas oscuro posible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased flex flex-col min-h-screen bg-gradient-to-b from-purple-950 via-purple-900 to-fuchsia-500 overflow-x-hidden">
+      <body className="font-body antialiased flex flex-col min-h-screen bg-gradient-to-b from-black via-purple-950 to-fuchsia-500 overflow-x-hidden">
         <ThemeManager />
         <Header />
         <div className="flex-grow flex flex-col">
