8435493f6fb85bfdb87e0e43762003caeae8def1
raul duarte <rauldrt5@gmail.com>
2025-08-08 16:50:36 +0000

    ya tenemos creado y firebase project

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,11 @@
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
+};
