5c8174b758b6f0f9f1c359302affffd800fc2212
raul duarte <rauldrt5@gmail.com>
2025-08-09 17:41:50 +0000

    ok, por ahora los formularios estan bien, tengo un lineamiento de colore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased flex flex-col min-h-screen bg-gradient-to-b from-black via-purple-950 to-fuchsia-500 overflow-x-hidden">
+      <body className="font-body antialiased flex flex-col min-h-screen bg-background text-foreground">
         <ThemeManager />
          <AuthProvider>
             <SocialModalProvider>
